42fe8f26fbd8960eb4909a3585070ee7948ac680
Alexander Herrera <Alex>
2023-06-10 19:40:18 -0400

    pagina comida favorita

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import imagen0 from './imagenes/imagen1.jpg';
+import imagen1 from './imagenes/imagen2.jpeg';
+import imagen2 from './imagenes/imagen3.jpeg';
+import imagen3 from './imagenes/imagen4.jpeg';
+import './miCss.css';
 
 function App() {
+  const nombres = ["Arroz Amarillo","Cerdo Asado", "Chicharrones", "Congris"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>"Comida Favorita"</h1>
+      <div className="contenedor-general">
+        <div className='contenedor-primario'>
+        <img 
+          src={imagen0}
+          alt='imagen0'
+        /> <br />
+        <p>{nombres[0]}</p>
+        </div> 
+        <div className='contenedor-primario'>
+        <img 
+          src={imagen1}
+          alt='imagen1'
+        /> <br />
+        <p>{nombres[1]}</p>
+        </div> 
+        <div className='contenedor-primario'>
+        <img 
+          src={imagen2}
+          alt='imagen2'
+        /> <br />
+        <p>{nombres[2]}</p>
+        </div> 
+        <div className='contenedor-primario'>
+        <img 
+          src={imagen3}
+          alt='imagen3'
+        /> <br />
+        <p>{nombres[3]}</p>
+        </div> 
+      </div>
     </div>
   );
 }
